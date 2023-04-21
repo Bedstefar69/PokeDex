ae58393345a81494b316104e5f30100a4beb5045
Bedstefar69 <andreastamsen@gmail.com>
2023-04-21 19:02:22 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Main from './components/Main';
 import './components/style.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import About from './components/About';
 function App() {
   return (
     <>
-      
+     
     </>
   );
 }
